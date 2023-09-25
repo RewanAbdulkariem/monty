95294f53f407ec81a53ec18787031f7c1f619258
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-25 09:29:35 +0300

    nop fun

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop -  doesn’t do anything
+ *
+ * @stack: Pointer to the stack.
+ * @line_number: The line number in the Monty file.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
